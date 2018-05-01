e9f50bb252526176679c46f7af0f65eef255eab7
leeicheng <s10243004@gmail.com>
2018-05-01 10:54:59 +0800

    update background and profile photo in my dog fragment.

--- app/src/main/java/com/example/leeicheng/dogbook/main/MainActivity.java
+++ app/src/main/java/com/example/leeicheng/dogbook/main/MainActivity.java
@@ -1,30 +1,50 @@
-package com.example.leeicheng.dogbook;
+package com.example.leeicheng.dogbook.main;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.example.leeicheng.dogbook.mydog.MyDogFragment;
+import com.example.leeicheng.dogbook.R;
+import com.example.leeicheng.dogbook.activities.ActivitiesFragment;
+import com.example.leeicheng.dogbook.articles.ArticlesFragment;
+import com.example.leeicheng.dogbook.chats.ChatsFragment;
+import com.example.leeicheng.dogbook.friends.FriendsFragment;
+
+import java.io.FileNotFoundException;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bnvMain;
+    Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
         findViews();
     }
 
-    void findViews(){
+    void findViews() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.flMain, new MyDogFragment()).commit();
         bnvMain = findViewById(R.id.bnvMain);
         bnvMain.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Fragment selectedFragment = null;
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.navMyDog:
-                        selectedFragment =  new MyDogFragment();
+                        selectedFragment = new MyDogFragment();
                         break;
                     case R.id.navActivities:
                         selectedFragment = new ActivitiesFragment();
@@ -39,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
                         selectedFragment = new ArticlesFragment();
                         break;
                 }
-                getSupportFragmentManager().beginTransaction().replace(R.id.flMain,selectedFragment).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.flMain, selectedFragment).commit();
                 return true;
             }
         });
